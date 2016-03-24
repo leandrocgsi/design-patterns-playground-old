d3d832e8667c71c231ab33fad4f78ef3353e6b0a
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-03-24 13:53:05 -0300

    Starting implementation of Fluent Interface example

---
+++ fluent-interface-pattern/src/main/java/br/com/erudio/EmailUtils.java
@@ -0,0 +1,116 @@
+package br.com.erudio;
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.MultiPartEmail;
+
+public class EmailUtils implements Serializable {
+
+	private String hostname;
+	private String username;
+	private String password;
+	private String from;
+	private int port;
+	private boolean ssl;
+	private String subject;
+	private String message;
+	
+	private MultiPartEmail getEmailSession(EMailConfigs configs) throws EmailException {
+		MultiPartEmail email = new MultiPartEmail();
+		email.setHostName(hostname);
+		email.setSmtpPort(port);
+		email.setAuthentication(username, password);
+		email.setSSL(ssl);
+		email.setFrom(from);
+		return email;
+	}
+
+	private EmailAttachment attachFile(String fileDir) {
+		File file = new File(fileDir);
+		EmailAttachment attachment = new EmailAttachment();
+		attachment.setPath(file.getPath());
+		attachment.setDisposition(EmailAttachment.ATTACHMENT);
+		attachment.setDescription("Attachment");
+		attachment.setName(file.getName());
+		return attachment;
+	}
+
+	private void sendSimpleMail(EMailConfigs configs, String to) throws EmailException, AddressException {
+		MultiPartEmail email = setConfigs(configs, to);
+		email.send();
+	}
+
+	private void sendEmailWithAttachment(EMailConfigs configs, String to, String filePath) throws EmailException, AddressException {
+		MultiPartEmail email = setConfigs(configs, to);
+		EmailAttachment attach = attachFile(filePath);
+		email.attach(attach);
+		email.send();
+	}
+
+	private MultiPartEmail setConfigs(EMailConfigs configs, String to) throws EmailException, AddressException {
+		MultiPartEmail email = new MultiPartEmail();
+		email = getEmailSession(configs);
+		email.setSubject(subject);
+		email.setMsg(message);
+		email.setTo(getRecipients(to));
+		return email;
+	}
+	
+	private ArrayList getRecipients(String to) throws AddressException {
+		String tosWithoutSpaces = to.replaceAll("\\s","");;
+		StringTokenizer tok = new StringTokenizer(tosWithoutSpaces,";");
+		ArrayList recipients = new ArrayList();
+		while(tok.hasMoreElements()) {
+			String string = tok.nextElement().toString();
+			recipients.add(new InternetAddress(string));
+		}
+		return recipients;
+	}
+
+	public EmailUtils setHostname(String hostname) {
+		this.hostname = hostname;
+		return this;
+	}
+
+	public EmailUtils setUsername(String username) {
+		this.username = username;
+		return this;
+	}
+
+	public EmailUtils setPassword(String password) {
+		this.password = password;
+		return this;
+	}
+
+	public EmailUtils setFrom(String from) {
+		this.from = from;
+		return this;
+	}
+
+	public EmailUtils setPort(int port) {
+		this.port = port;
+		return this;
+	}
+
+	public EmailUtils setSsl(boolean ssl) {
+		this.ssl = ssl;
+		return this;
+	}
+
+	public EmailUtils setSubject(String subject) {
+		this.subject = subject;
+		return this;
+	}
+
+	public EmailUtils setMessage(String message) {
+		message = message;
+		return this;
+	}
+}
