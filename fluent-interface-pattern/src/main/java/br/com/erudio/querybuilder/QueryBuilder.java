dd992c4f12d5a28cbbb858e32787a68a809b1853
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-09 16:36:59 -0300

    Implementing a query builder

--- fluent-interface-pattern/src/main/java/br/com/erudio/querybuilder/QueryBuilder.java
+++ fluent-interface-pattern/src/main/java/br/com/erudio/querybuilder/QueryBuilder.java
@@ -7,9 +7,11 @@ import org.apache.commons.lang3.StringUtils;
 
 public class QueryBuilder<T extends Serializable> implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
 	private PagedSearchVO<T> pagedSearchVO = new PagedSearchVO<T>();
 	
-	public QueryBuilder withVO(PagedSearchVO<T> pagedSearchVO) {
+	public QueryBuilder<T> withVO(PagedSearchVO<T> pagedSearchVO) {
 		this.pagedSearchVO = pagedSearchVO;
 		return this;
 	}
@@ -21,11 +23,14 @@ public class QueryBuilder<T extends Serializable> implements Serializable {
 	public String getWhereAndParameters(String alias) {
 		String query = " where ";
 		
-		for (Map.Entry<String, Object> entry : pagedSearchVO.getFilters().entrySet()){
-		    String k = entry.getKey();
-			Object v = entry.getValue();
-		    if (entryIsEmpty(k, v)) {
-				query = query + alias + "." + k + " = :" + k + " and ";
+		Map<String, Object> filters = pagedSearchVO.getFilters();
+		if (filters != null) {
+			for (Map.Entry<String, Object> entry : filters.entrySet()){
+			    String k = entry.getKey();
+				Object v = entry.getValue();
+			    if (entryIsEmpty(k, v)) {
+					query = query + alias + "." + k + " = :" + k + " and ";
+				}
 			}
 		}
 		return query + "1 = 1 ";
